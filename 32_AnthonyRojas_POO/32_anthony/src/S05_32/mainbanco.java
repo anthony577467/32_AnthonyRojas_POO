c161bf126778cde710adeec093da57d516abdafe
anthony577467 <136906838+anthony577467@users.noreply.github.com>
2023-09-30 08:30:49 -0500

    mainbanco.java

--- 32_AnthonyRojas_POO/32_anthony/src/S05_32/mainbanco.java
+++ 32_AnthonyRojas_POO/32_anthony/src/S05_32/mainbanco.java
@@ -4,11 +4,15 @@
  * and open the template in the editor.
  */
 package S05_32;
-
-/**
- *
- * @author USUARIO
- */
+import java.util.Scanner;
 public class mainbanco {
-    
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("titular de la cuenta: ");
+        String titular = scanner.nextLine();
+
+        System.out.print("Ingrese el saldo de la cuenta: ");
+        double saldoInicial = scanner.nextDouble();
+}
 }
