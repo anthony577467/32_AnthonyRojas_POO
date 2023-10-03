ba07ff43d2e7b2825850142a89f9249a990adda9
anthony577467 <136906838+anthony577467@users.noreply.github.com>
2023-10-03 08:53:26 -0500

    Add files via upload

---
+++ s07_32AnthonyRojas/persona1.java
@@ -0,0 +1,13 @@
+
+package s07_32AnthonyRojas;
+
+
+public class persona1 {
+
+    // Método con parámetros
+    public void saludar(String nombre) {
+        System.out.println("Hola " + nombre + "!");
+    }
+
+}
+
